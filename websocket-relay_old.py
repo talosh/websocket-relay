665ec2448f066c89e7a1d055495f1539a5d40fa6
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-06-16 22:11:32 +0100

    update view-stream.html and websocket-relay.py

--- websocket-relay_old.py
+++ websocket-relay_old.py
@@ -12,14 +12,13 @@ from tornado.options import define, options
 define("port", default=8888, help="run on the given port", type=int)
 define("secret", help="upstream secret token", type=str)
 
-secret_to_url = {}
 
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
             (r"/", MainHandler),
             (r"/upload/(.*)", StreamHandler),
-            (r"/live/(.*)", SocketHandler),
+            (r"/live.ts", SocketHandler),
         ]
         settings = dict(
             template_path=os.path.join(os.path.dirname(__file__), 'templates'),
@@ -36,26 +35,28 @@ class MainHandler(tornado.web.RequestHandler):
 @tornado.web.stream_request_body
 class StreamHandler(tornado.web.RequestHandler):
     def data_received(self, data):
-        secret = self.request.path.split('/')[-1]
-        if secret not in secret_to_url:
-            logging.info('Failed Stream Connection: %s - wrong secret.', self.request.remote_ip)
+        if options.secret and self.request.path != '/upload/' + options.secret:
+            logging.info(
+                'Failed Stream Connection: %s - wrong secret.',
+                self.request.remote_ip
+            )
             self.write_error(403)
             return
-        url = secret_to_url[secret]
-        SocketHandler.broadcast(data, url)
+        SocketHandler.broadcast(data)
 
 
 class SocketHandler(tornado.websocket.WebSocketHandler):
-    waiters = {}
+    waiters = set()
 
     def check_origin(self, origin):
         return True
 
-    def open(self, url):
-        if url not in SocketHandler.waiters:
-            SocketHandler.waiters[url] = set()
-        SocketHandler.waiters[url].add(self)
-        logging.info(f'New WebSocket Connection for {url}: {len(SocketHandler.waiters[url])} total')
+    def open(self):
+        SocketHandler.waiters.add(self)
+        logging.info(
+            'New WebSocket Connection: %d total',
+            len(SocketHandler.waiters)
+        )
 
     def select_subprotocol(self, subprotocol):
         if len(subprotocol):
@@ -66,28 +67,23 @@ class SocketHandler(tornado.websocket.WebSocketHandler):
         pass
 
     def on_close(self):
-        for waiters in SocketHandler.waiters.values():
-            if self in waiters:
-                waiters.remove(self)
-                logging.info('Disconnected WebSocket: %d total', len(waiters))
+        SocketHandler.waiters.remove(self)
+        logging.info(
+            'Disconnected WebSocket (%d total)',
+            len(SocketHandler.waiters)
+        )
 
     @classmethod
-    def broadcast(cls, data, url):
-        if url in cls.waiters:
-            for waiter in cls.waiters[url]:
-                try:
-                    waiter.write_message(data, binary=True)
-                except tornado.websocket.WebSocketClosedError:
-                    logging.error("Error sending message", exc_info=True)
+    def broadcast(cls, data):
+        for waiter in cls.waiters:
+            try:
+                waiter.write_message(data, binary=True)
+            except tornado.websocket.WebSocketClosedError:
+                logging.error("Error sending message", exc_info=True)
+
 
 def main():
     tornado.options.parse_command_line()
-
-    if options.secrets:
-        for secret in options.secrets:
-            # Associate each secret with a corresponding URL
-            secret_to_url[secret] = f'live/{secret}'
-
     app = Application()
     app.listen(options.port)
     tornado.ioloop.IOLoop.current().start()
